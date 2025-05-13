32bd1d633b9fe390144072fda09b65da0939231b
SalmaFennan <salmafennan19@gmail.com>
2025-05-13 19:43:08 +0200

    first commit

---
+++ src/main/java/ma/enset/ebankingbackend/entities/SavingAccount.java
@@ -0,0 +1,21 @@
+package ma.enset.ebankingbackend.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@DiscriminatorValue("SA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavingAccount extends BankAccount {
+    private double interestRate;
+    private Long id;
+
+
+}
